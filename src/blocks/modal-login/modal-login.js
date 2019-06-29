07b5e5f87135b30e6e8b4d2bc065550433478546
Roman T <tselenkor@gmail.com>
2019-06-29 13:52:56 +0300

    fixes

--- src/blocks/modal-login/modal-login.js
+++ src/blocks/modal-login/modal-login.js
@@ -9,18 +9,16 @@ const checkForm = () => {
     const isValidatedPhone = mobile.value.match(myRegExpPhone) ? true : false
     const isValidatedPassword = password.value.length>=5 ? true : false
     const isValidatePhoneAndPassword = () => isValidatedPhone && isValidatedPassword ? true : false
-     
+    
    if (isValidatePhoneAndPassword()) {
     submitButton.style = 'background-color:none; color:white; border:1px solid white; opacity:0.9;'
-    submitButton.disabled=false
-
+    submitButton.disabled=false     
    } else {
        submitButton.style = 'background-color:none; color:gray; border:1px solid gray; opacity:0.2;'       
-       submitButton.disabled=true
-   }
-   
+       submitButton.disabled=true        
+   }   
 } 
     
 mobile.addEventListener('input', () =>checkForm()) 
- password.addEventListener('input', () =>checkForm()) 
+password.addEventListener('input', () =>checkForm()) 
  
